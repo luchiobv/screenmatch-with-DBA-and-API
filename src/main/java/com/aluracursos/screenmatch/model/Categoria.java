fd8bd22472f58cb2dd1119c6c83d36e84feb2cba
Luis <luchobava93@gmail.com>
2024-05-08 15:32:30 -0500

    Project screenmatch  using Springgboot, JPA , DBA posgrest and API

---
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.aluracursos.screenmatch.model;
+
+import java.text.Normalizer;
+
+public enum Categoria {
+    ACCION("Action", "Accion"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comedia"),
+    DRAMA("Drama", "Drama"),
+    CRIMEN("Crime", "Crimen");
+
+    private String categoriaOmdb;
+    private String categoriaEspanol;
+    Categoria (String categoriaOmdb, String categoriaEspanol){
+        this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol = categoriaEspanol;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+
+    public static Categoria fromEspanol(String text) {
+        //verified if the text that took from categoria is equal to the category.
+        for (Categoria categoria : Categoria.values()) {
+
+            if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+}
---
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+//    public static String obtenerTraduccion(String texto) {
+//        OpenAiService service = new OpenAiService("sk-proj-gzmhvhoQipJ0bJoeNwY8T3BlbkFJ2JZmq4NQXaMr4d2VkuN6");
+//
+//        CompletionRequest requisicion = CompletionRequest.builder()
+//                .model("gpt-3.5-turbo-instruct")
+//                .prompt("traduce a español el siguiente texto: " + texto)
+//                .maxTokens(1000)
+//                .temperature(0.7)
+//                .build();
+//
+//        var respuesta = service.createCompletion(requisicion);
+//        return respuesta.getChoices().get(0).getText();
+//    }
+}
